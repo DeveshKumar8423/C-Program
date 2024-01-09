f85455828e2390e0b9101e4de205eaa733c0afc1
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-01-09 23:31:04 +0530

    simple interest

---
+++ Extra Codes/a.c
@@ -0,0 +1,12 @@
+// Program of Simple Interest
+#include<stdio.h>
+int main(){
+    int p , n;
+    float r ,  si;
+    p = 500;
+    n = 2;
+    r = 7;
+    si = p*n*r/100;
+    printf("%f\n", si);
+    return 0;
+}
